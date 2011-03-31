20e1b5fca43f8df7b6d86f5c7416e50ca2f979bc
marc fuentes <marc.fuentes@inria.fr>
2011-03-31 21:24:26 +0200

    improve conrec.pyx

--- conrec.pyx
+++ conrec.pyx
@@ -8,10 +8,8 @@
 #   z               ! contour levels in increasing order
 #
 
-cdef inline double xsect(p1,p2):
-    return (h[p2]*xh[p1]-h[p1]*xh[p2])/(h[p2]-h[p1])
-cdef inline double ysect(p1,p2):
-    return (h[p2]*yh[p1]-h[p1]*yh[p2])/(h[p2]-h[p1])
+cdef inline double xsect(double xh[5], double h[5], int p1, int p2): return (h[p2]*xh[p1]-h[p1]*xh[p2])/(h[p2]-h[p1]) 
+cdef inline double ysect(double yh[5], double h[5], int p1, int p2): return (h[p2]*yh[p1]-h[p1]*yh[p2])/(h[p2]-h[p1])
 cdef inline double MIN(double a, double b) : return a if a <= b  else b
 cdef inline double MAX(double a, double b) : return b if a <= b  else a 
 
@@ -22,7 +20,6 @@ cimport numpy as nc
 def Contour(nc.ndarray[double,ndim=2] d,int ilb,int iub,int jlb,int jub,
             nc.ndarray[double,ndim = 1] x,
             nc.ndarray[double,ndim = 1] y,
-            int nc, 
             nc.ndarray[double,ndim = 1] z):
 
    cdef int m1,m2,m3,case_value
@@ -33,7 +30,7 @@ def Contour(nc.ndarray[double,ndim=2] d,int ilb,int iub,int jlb,int jub,
    cdef double xh[5], yh[5]
    cdef int *im = [0, 1, 1, 0]
    cdef int *jm = [0, 0, 1, 1]
-   cdef nc.ndarray[double,ndim=3] castab = np.array(
+   cdef nc.ndarray[int,ndim=3] castab = np.array(
        [[[0,0,8], [0,2,5], [7,6,9]],
        [[0,3,4], [1,3,1], [4,3,0]],
        [[9,6,7], [5,2,0], [8,0,0]]])
@@ -48,9 +45,9 @@ def Contour(nc.ndarray[double,ndim=2] d,int ilb,int iub,int jlb,int jub,
          temp1 = MAX(d[i][j],d[i][j+1])
          temp2 = MAX(d[i+1][j],d[i+1][j+1])
          dmax  = MAX(temp1,temp2);
-         if (dmax < z[0]) or ( dmin > z[nc-1]):
+         if (dmax < z[0]) or ( dmin > z[-1]):
             continue
-         for k from 0<= k <nc:
+         for k in range(z.shape[0]):
             if (z[k] < dmin ) or ( z[k] > dmax):
                continue
             for m from 4>= m >=0: 
@@ -76,59 +73,21 @@ def Contour(nc.ndarray[double,ndim=2] d,int ilb,int iub,int jlb,int jub,
                   m3 = m + 1
                else:
                   m3 = 1
-               #if ((case_value = castab[sh[m1]+1][sh[m2]+1][sh[m3]+1]) == 0):
-               #   continue
-               switch (case_value) 
-               if case_value=1: # Line between vertices 1 and 2 
-                   x1 = xh[m1]
-                   y1 = yh[m1]
-                   x2 = xh[m2]
-                   y2 = yh[m2]
-                   break
-                elif case_value=2: # Line between vertices 2 and 3 
-                   x1 = xh[m2];
-                   y1 = yh[m2];
-                   x2 = xh[m3];
-                   y2 = yh[m3];
-                   break;
-               elif case_value= 3: # Line between vertices 3 and 1 
-                   x1 = xh[m3];
-                   y1 = yh[m3];
-                   x2 = xh[m1];
-                   y2 = yh[m1];
-                   break;
-               elif case_value= 4: # Line between vertex 1 and side 2-3 
-                   x1 = xh[m1];
-                   y1 = yh[m1];
-                   x2 = xsect(m2,m3);
-                   y2 = ysect(m2,m3);
-                   break;
-               elif case_value= 5: # Line between vertex 2 and side 3-1 
-                   x1 = xh[m2];
-                   y1 = yh[m2];
-                   x2 = xsect(m3,m1);
-                   y2 = ysect(m3,m1);
-               elif case_value= 6: # Line between vertex 3 and side 1-2 */
-                   x1 = xh[m3];
-                   y1 = yh[m3];
-                   x2 = xsect(m1,m2);
-                   y2 = ysect(m1,m2);
-               elif case_value= 7: # Line between sides 1-2 and 2-3 */
-                   x1 = xsect(m1,m2);
-                   y1 = ysect(m1,m2);
-                   x2 = xsect(m2,m3);
-                   y2 = ysect(m2,m3);
-               elif case_value= 8: # Line between sides 2-3 and 3-1 */
-                   x1 = xsect(m2,m3);
-                   y1 = ysect(m2,m3);
-                   x2 = xsect(m3,m1);
-                   y2 = ysect(m3,m1);
-               elif case_value= 9: # Line between sides 3-1 and 1-2 */
-                   x1 = xsect(m3,m1);
-                   y1 = ysect(m3,m1);
-                   x2 = xsect(m1,m2);
-                   y2 = ysect(m1,m2);
-               end 
-
-               # Finally draw the line */
-               ConrecLine(x1,y1,x2,y2,z[k]);
+               case_value = castab[sh[m1]+1,sh[m2]+1,sh[m3]+1]
+               
+               if (case_value != 0):
+                  [x1, y1, x2, y2] =\
+                  {1: lambda v:[xh[m1], yh[m1], xh[m2], yh[m2]], # Line between vertices 1 and 2  
+                   2: lambda v:[xh[m2], yh[m2], xh[m3], yh[m3]], # Line between vertices 2 and 3 
+                   3: lambda v:[xh[m3], yh[m3], xh[m1], yh[m1]], # Line between vertices 3 and 1 
+                   4: lambda v:[xh[m1], yh[m1], xsect(xh, h, m2, m3), ysect(yh, h, m2, m3)], # Line between vertex 1 and side 2-3 
+                   5: lambda v:[xh[m2], yh[m2], xsect(xh, h, m3, m1), ysect(yh, h, m3, m1)], # Line between vertex 2 and side 3-1 
+                   6: lambda v:[xh[m3], yh[m3], xsect(xh, h, m1, m2), ysect(yh, h, m1, m2)], # Line between vertex 3 and side 1-2 
+                   7: lambda v:[xsect(xh, h, m1, m2), ysect(yh, h, m1, m2), xsect(xh, h, m2, m3), ysect(yh, h, m2,m3)], # Line between sides 1-2 and 2-3 
+                   8: lambda v:[xsect(xh, h, m2, m3), ysect(yh, h, m2, m3), xsect(xh, h, m3, m1), ysect(yh, h, m3,m1)], # Line between sides 2-3 and 3-1 
+                   9: lambda v:[xsect(xh, h, m3, m1), ysect(yh, h, m3, m1), xsect(xh, h, m1, m2), ysect(yh, h, m1,m2)]  # Line between sides 3-1 and 1-2 
+                  }[case_value](1)
+               
+                  # Finally draw the line */
+                  print "line (%f,%f,%f,%f) z = %f" %(x1,y1,x2,y2,z[k])
+                  #ConrecLine(x1,y1,x2,y2,z[k]);
